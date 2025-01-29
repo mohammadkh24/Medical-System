6a549e285979679d790f7d0274d2eea595cf657f
Mohammad <mohammadgithub2024@gmail.com>
2025-01-29 18:33:13 +0330

    add features : users

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,77 @@
+const { isValidObjectId } = require("mongoose");
 const usersModel = require("../models/users");
 
-exports.getAll = async (req,res) => {
-    const users = await usersModel.find({}).lean();
+exports.getAll = async (req, res) => {
+  const users = await usersModel.find({}).lean();
 
-    return res.json(users)
-}
-exports.getOne = async (req,res) => {
+  return res.json(users);
+};
+exports.getOne = async (req, res) => {
+  const { id } = req.params;
 
-}
-exports.changeRole = async (req,res) => {
+  if (!isValidObjectId(id)) {
+    return res.status(400).json({
+      message: "UserID is not valid !!",
+    });
+  }
 
-}
-exports.remove = async (req,res) => {
+  const user = await usersModel.findOne({ _id: id }).select("-password");
 
-}
+  if (!user) {
+    return res.status(404).json({
+      message: "User Not Found !!",
+    });
+  }
+
+  return res.json(user);
+};
+
+exports.changeRole = async (req, res) => {
+  const { id } = req.params;
+  const { role } = req.body;
+
+  if (!isValidObjectId(id)) {
+    return res.status(400).json({
+      message: "UserID is not valid !!",
+    });
+  }
+
+  const changeRole = await usersModel.findOneAndUpdate(
+    { _id: id },
+    {
+      role,
+    }
+  );
+
+  if (!changeRole) {
+    return res.status(404).json({
+      message: "User Not Found !!",
+    });
+  }
+
+  return res.status(200).json({
+    message: "User Role Changed Successfully",
+  });
+};
+
+exports.remove = async (req, res) => {
+  const { id } = req.params;
+
+  if (!isValidObjectId(id)) {
+    return res.status(400).json({
+      message: "UserID is not valid !!",
+    });
+  }
+
+  const removeUser = await usersModel.findOneAndDelete({ _id: id });
+
+  if (!removeUser) {
+    return res.status(404).json({
+      message: "User Not Found !!",
+    });
+  }
+
+  return res.status(201).json({
+    message: "User Removed Successfully",
+  });
+};
